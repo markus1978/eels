cf9f6972cb89c07fa611f2a7c9f81fcc07063176
Markus Scheidgen <markus.scheidgen@gmail.com>
2020-03-02 12:52:18 +0100

    Fixed setup.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def main():
         license='APACHE 2.0',
         packages=find_packages(),
         package_data={
-            'eels': ['*.json']
+            'eelsparser': ['*.json']
         },
         install_requires=[
             'nomadcore'
